ba014e74b4e41b23680c6ed22a0e867c9f0135af
lateefaayesufu <yesufulateefah@gmail.com>
2026-04-22 08:52:10 +0100

    feat: add invoiceSlice for managing invoice state and async actions

---
+++ src/redux/invoice/invoiceSlice.ts
@@ -0,0 +1,45 @@
+import { PayloadAction, createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { InvoiceType } from '../../types/InvoiceTypes';
+import localDB from '../../services/localStorage';
+
+interface stateType {
+  invoiceItems: InvoiceType[];
+  loading: boolean;
+  sortedInvoice: InvoiceType[];
+}
+
+const initialState: stateType = {
+  invoiceItems: [],
+  loading: false,
+  sortedInvoice: [],
+};
+
+const invoiceSlice = createSlice({
+  name: 'invoice',
+  initialState,
+  reducers: {
+    updateSortedInvoice: (state, action: PayloadAction<InvoiceType[]>) => {
+      state.sortedInvoice = action.payload;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getInvoiceAsync.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(getInvoiceAsync.fulfilled, (state, action: PayloadAction<InvoiceType[]>) => {
+        state.loading = false;
+        state.invoiceItems = [...action.payload];
+      });
+  },
+});
+
+export const getInvoiceAsync = createAsyncThunk(
+  'invoice/getInvoiceAsync',
+  async () => {
+    return localDB.getAll();
+  }
+);
+
+export const { updateSortedInvoice } = invoiceSlice.actions;
+export default invoiceSlice.reducer;
